376ccf137622b369d7138cd20258ea12254c224a
benracine <benracinedev@gmail.com>
2021-01-09 15:13:35 -0700

    various modest mods... mostly making it a bit more OO, but still trying to maintain mostly pure methods

--- countStates.js
+++ countStates.js
@@ -1,49 +1,56 @@
 const { assert } = require('console');
 const fs = require('fs');
 const { EOL } = require('os'); 
+const util = require('util')
 const _ = require('underscore');
+const readFilePromise = util.promisify(fs.readFile);
 
-/*
-I wrote this code just to toy around with a hybrid OO / FP coding style
-*/
+const createStateFinder = (opts={}) => {
+  let { stateFileName = null, textFileName = null} = opts;
 
-const createStateFinder = (statesFile) => {
+  // Maximize pure methods/functions as these are easy to test
   const descendingCount = (pair) => -1 * pair[1];
   const pairToStr = (pair) => `${pair[0]} ${pair[1]}`; 
-  const pairToObj = (pair) => {
-    let obj = {};
-    obj[pair[0]] = pair[1];
-    return obj;
-  };
-  const extendObjByPair = (memo, pair) => { 
-    return {...memo, ...pairToObj(pair)};
-  };
-  const createMapping = (ABBRS, STATES) => {
-    return _.zip(ABBRS, STATES).reduce(extendObjByPair); 
-  };
 
-  const utils = (() => {
-    const { STATES, ABBRS } = require(statesFile);
-    const UNION = [...STATES, ...ABBRS];
+  const getUtils = () => {
+    if (!stateFileName) return;
+    const { states, abbrs } = require(stateFileName);
+    const statesAndAbbrs = [...states, ...abbrs];
+    const extendObjByPair = (memo, pair) => { 
+      return {...memo, ...Object.fromEntries([pair])};
+    };
+    const createMapping = (abbrs, states) => {
+      return _.zip(abbrs, states).reduce(extendObjByPair); 
+    };
     return {
-      lineContainsState: (line) => RegExp(UNION.join('|')).test(line),
+      lineContainsState: (line) => RegExp(statesAndAbbrs.join('|')).test(line),
       extractStateFromLine: (line) => {
         const addState = (memo, state) => { 
           return line.includes(state)
             ? [...memo, [state]]
             : memo;
         };
-        return UNION.reduce(addState, []);
+        return statesAndAbbrs.reduce(addState, []);
       },
       expandAbbreviations: (state) => {
-        if (STATES.includes(state)) return state;
-        else if (ABBRS.includes(state)) return createMapping(ABBRS, STATES)[state];
-        else return state;
+        if (states.includes(state)) {
+          return state;
+        }
+        else if (abbrs.includes(state)) {
+          return createMapping(abbrs, states)[state];
+        }
+        else {
+          return state;
+        }
       }
     }
-  })();
-  
+  };
+
   const processString = (entireString) => {
+    // Lazily using getUtils allow the tool be re-used on various
+    // text files and various definitions of what qualifies as a 
+    // state or valid abbreviation
+    const utils = getUtils(); 
     return _.chain(entireString.split(EOL))
       .filter(utils.lineContainsState)
       .map(utils.extractStateFromLine)
@@ -56,26 +63,56 @@ const createStateFinder = (statesFile) => {
       .value();
   };
 
-  return {
-    processString,
-  }
+  const yieldInterface = () => {
+    return {
+      processString,
+      countStates,
+      stateFileName,
+      setStateFileName,
+      textFileName,
+      setTextFileName
+    }
+  };
+  // End pure methods/functions
+
+  // Begin impure methods/functions
+  // Try to minimize the functionality herein as testing is more work
+  const countStates = async () => {
+    if (!textFileName) {
+      return 'No file name available';
+    }
+    return processString(
+      await readFilePromise(textFileName, 'utf8')
+    );
+  };
+
+  const setStateFileName = (fName) => {
+    stateFileName = fName;
+    return yieldInterface();
+  };
+
+  const setTextFileName = (fName) => {
+    textFileName = fName;
+    return yieldInterface();
+  };
+  // End impure methods/functions
+
+  return yieldInterface();
 };
 
-// Impure functions
-const findStates = (err, fileContents) => {
-  let stateFinder = createStateFinder('./states.json');
-  let results = stateFinder.processString(fileContents);
-  results.forEach((line) => console.log(line));
-  runTests(results);
+const main = async () => {
+  return await createStateFinder()
+    .setStateFileName('./states.json')
+    .setTextFileName('./homesteading-cities.txt')
+    .countStates();
 };
 
 const runTests = (results) => {
+  console.log('results = ', results);
   assert(results[0] === 'Tennessee 9');
   assert(results[results.length - 1] === 'New York 1');
 };
 
-const main = (filename) => fs.readFile(filename, 'utf8', findStates); 
-
 if (require.main === module) {
-  main('./homesteading-cities.txt');
+  main().then(runTests);
 }
--- readme.md
+++ readme.md
@@ -1,4 +1,4 @@
-## Functional purity
+## Functional purity meets OO
 
 This is just a little exploration into how easy or hard it is to separate JavaScript(/node) code into a collection of mostly pure little functions.
 
@@ -13,9 +13,3 @@ I also wrapped it up into what is kinda sorta OO code, but not really.
 `$ npm install`
 
 `$ npm start`
-
-## TODO
-
-Things I really should add:
-- Tests
-
